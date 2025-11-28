e7acf5edcad7df14dc6128cd48d76dbb7cc72942
mohamedthowfiq <mohamedthowfiq9626@gmail.com>
2025-11-28 14:22:34 +0530

    test_income_cal upgraded to fixtures

--- conftest.py
+++ conftest.py
@@ -1,17 +1,22 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 
 # @pytest.fixture(params=["chrome","firefox","edge"])
-@pytest.fixture(params=["chrome","edge"])
+# @pytest.fixture(params=["chrome","edge"])
+@pytest.fixture(params=["chrome"])
 def initialize_driver(request):
-  if request.param == "chrome":
-    driver = webdriver.Chrome()
-  # elif request.param == "firefox":
-  #   driver = webdriver.Firefox()
-  elif request.param == "edge":
-    driver = webdriver.Edge()
-  request.cls.driver = driver  # connects driver to class
-  print("Browser: ",request.param) 
-  yield
-  print("Close driver")
-  driver.close()
+    if request.param == "chrome":
+      driver = webdriver.Chrome()
+    # elif request.param == "firefox":
+    #   driver = webdriver.Firefox()
+    # elif request.param == "edge":
+    #   driver = webdriver.Edge()
+
+    request.cls.driver = driver  # connects driver to class
+    request.cls.wait = WebDriverWait(driver, 10, poll_frequency=2)# create WebDriverWait and attach to class
+    
+    print(f"Browser: {request.param}") 
+    yield
+    print("Close driver")
+    driver.close()
--- income_calculator_package/test_income_calculator.py
+++ income_calculator_package/test_income_calculator.py
@@ -1,47 +1,44 @@
-def test_income_calculator():################################################################################################
 
-  from selenium import webdriver
-  from selenium.webdriver.chrome.service import Service
-  from selenium.webdriver.chrome.options import Options
-  from selenium.webdriver.support.ui import WebDriverWait
-  from selenium.webdriver.support import expected_conditions as EC
-  from selenium.webdriver.common.by import By
-
-  options = Options()
-  options.add_experimental_option("detach",True)
-  options.add_argument("--start-maximized")
-
-  driver = webdriver.Chrome(options=options)
-
-  wait = WebDriverWait(driver,10,poll_frequency=2,ignored_exceptions=[Exception])
-
-  print("line 1")
-
-  driver.get("https://payrup.com/")
-
-  become_a_merchant = wait.until(EC.element_to_be_clickable((By.XPATH,"//a[text()='Become a Merchant']")))
-  become_a_merchant.click()
-
-  print("line 2")
-
-  # SWITCH TO NEW TAB
-  driver.switch_to.window(driver.window_handles[1])
-  print("Switched to new tab")
-  print("line 3")
-  income_cal = wait.until(EC.element_to_be_clickable((By.XPATH,"//button[text()='Income Calculator']")))
-  print("line 4")
-  income_cal.click()
-  print("line 5")
-
-  for i in range(3):
-    no_of_transaction = wait.until(EC.element_to_be_clickable((By.XPATH,f"//input[@id=':r{i}:']")))
-    no_of_transaction.send_keys("4")
-  print("line 8")
-  cal_btn = wait.until(EC.element_to_be_clickable((By.XPATH,"//button[text()='Calculate']")))
-  cal_btn.click()
-  print("line 9")
+import pytest,time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
+@pytest.mark.usefixtures("initialize_driver")
+class BaseClass:
+    pass
+
+class Test_class_income_calculator(BaseClass):
+  def test_income_calculator(self):
+
+      
+
+      self.driver.get("https://payrup.com/")
+
+      become_a_merchant = self.wait.until(
+          EC.element_to_be_clickable((By.XPATH, "//a[text()='Become a Merchant']"))
+      )
+      become_a_merchant.click()
+
+      # SWITCH TO NEW TAB
+      self.driver.switch_to.window(self.driver.window_handles[1])
+      print("Switched to new tab")
+      time.sleep(3)
+      income_cal = self.wait.until(
+          EC.element_to_be_clickable((By.XPATH, "//button[text()='Income Calculator']"))
+      )
+      income_cal.click()
+      time.sleep(3)
+      for i in range(3):
+          no_of_transaction = self.wait.until(
+              EC.element_to_be_clickable((By.XPATH, f"//input[@id=':r{i}:']"))
+          )
+          no_of_transaction.send_keys("4")
+
+      cal_btn = self.wait.until(
+          EC.element_to_be_clickable((By.XPATH, "//button[text()='Calculate']"))
+      )
+      cal_btn.click()
 
 
 
