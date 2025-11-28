dc3afdd53cd6b760deff091fa713705ff71e4b4e
mohamedthowfiq <mohamedthowfiq9626@gmail.com>
2025-11-28 11:48:17 +0530

    paramterize

---
+++ demoPytest/test_parameters_2.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+
+@pytest.mark.usefixtures("initialize_driver")
+class BaseClass:
+  pass
+class Test_Drivers(BaseClass):
+  def test_multiple_browsers(self):
+      self.driver.get("https://payrup.com/")
+      title = self.driver.title
+      print("Title :", title)
+      assert title == "Safe & Secure Mobile Recharges & Bill Payment"
